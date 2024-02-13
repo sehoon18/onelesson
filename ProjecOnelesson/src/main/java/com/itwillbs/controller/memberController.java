fc49961bbcce1cf59d84980c7c2c46f5dd63c5b8
양재민 <@DESKTOP-EO306BM>
2024-02-13 17:53:19 +0900

    마이페이지 배너, 마이 레슨 배너

--- ProjecOnelesson/src/main/java/com/itwillbs/controller/memberController.java
+++ ProjecOnelesson/src/main/java/com/itwillbs/controller/memberController.java
@@ -3,8 +3,10 @@ package com.itwillbs.controller;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.itwillbs.domain.MemberDTO;
 import com.itwillbs.service.MemberService;
 
 @Controller
@@ -13,6 +15,24 @@ public class memberController {
 	@Inject
 	private MemberService memberService;
 	
+	@GetMapping("/mypage")
+	public String mypage(MemberDTO memberDTO) {
+		System.out.println("MemberController mypage()");
+		
+		return "member/mypage";
+	}//mypage()
 	
-
+	@GetMapping("/myLessonList")
+	public String myLessionList(MemberDTO memberDTO) {
+		System.out.println("MemberController myLessionList()");
+		
+		return "member/myLessonList";
+	}//myLessionList();
+	
+	@GetMapping("/myInfo")
+	public String myInfo(MemberDTO memberDTO) {
+		System.out.println("MemberController myInfo()");
+		
+		return "member/myInfo";
+	}//myInfo();
 }
